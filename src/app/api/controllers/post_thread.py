f29b8512ca57633ddc4ee085a1d88385332e93b6
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-11-08 00:50:51 +0900

    [fix] スレッド登録APIを修正

--- src/app/api/controllers/post_thread.py
+++ src/app/api/controllers/post_thread.py
@@ -1,3 +1,6 @@
+import uuid
+from datetime import datetime
+
 from flask import Blueprint, jsonify, request
 
 from app.api.schemas import ThreadSchema
@@ -8,9 +11,24 @@ bp = Blueprint("post_thread", __name__)
 
 @bp.route("/threads", methods=["POST"])
 def post_thread():
-    requestBody = request.get_json()
-    thread = ThreadSchema().load(requestBody)
+    request_body = request.get_json()
+    thread = _deserialize(request_body)
+
     thread.save()
 
-    responseBody = ThreadSchema().dump(thread)
-    return (jsonify(responseBody), 201)
+    response_body = _serialize(thread)
+    return (jsonify(response_body), 201)
+
+
+def _deserialize(request_body):
+    thread = ThreadSchema().load(request_body)
+
+    utcnow = datetime.utcnow()
+    thread.id = str(uuid.uuid4())
+    thread.created_at = utcnow
+    thread.updated_at = utcnow
+    return thread
+
+
+def _serialize(model):
+    return ThreadSchema().dump(model)
