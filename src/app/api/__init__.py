7e8cb786b8b5996d58838525a7887580b35cc016
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-11-10 14:01:12 +0900

    [fix] リクエスト受付・終了時にログを出すようにした

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -2,6 +2,12 @@ from marshmallow import ValidationError
 
 from app.api.controllers import __all__ as blueprints
 from app.api.error_handlers import application_error_handler, validation_error_handler
+from app.api.hooks import (
+    after_request_handlers,
+    before_request_handlers,
+    teardown_appcontext_handlers,
+    teardown_request_handlers,
+)
 
 
 def register_blueprint(app):
@@ -12,3 +18,10 @@ def register_blueprint(app):
 def register_error_handler(app):
     app.register_error_handler(ValidationError, validation_error_handler)
     app.register_error_handler(Exception, application_error_handler)
+
+
+def configure_hooks(app):
+    app.before_request(before_request_handlers)
+    app.after_request(after_request_handlers)
+    app.teardown_request(teardown_request_handlers)
+    app.teardown_appcontext(teardown_appcontext_handlers)
