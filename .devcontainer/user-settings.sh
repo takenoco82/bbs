563f792878104f7de84595d46661455c1110f454
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-11-16 12:46:42 +0900

    [del] user-settings.shをGit管理対象外にした

--- .devcontainer/user-settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-readonly SCRIPT_DIR=$(cd $(dirname $0); pwd)
-readonly PROJECT_DIR=$(cd ${SCRIPT_DIR}; git rev-parse --show-toplevel)
-# home directory of the host machine
-readonly LOCALHOST_HOME=/localhost_home
