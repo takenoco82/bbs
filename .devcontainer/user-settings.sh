eb3a7b208708a9f227f12ffb0869907b3d289ac9
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-11-16 12:41:05 +0900

    [fix] user-settings.shを修正

--- .devcontainer/user-settings.sh
+++ .devcontainer/user-settings.sh
@@ -2,9 +2,7 @@
 
 set -eux
 
+readonly SCRIPT_DIR=$(cd $(dirname $0); pwd)
+readonly PROJECT_DIR=$(cd ${SCRIPT_DIR}; git rev-parse --show-toplevel)
 # home directory of the host machine
-LOCALHOST_HOME=/localhost_home
-
-# copy git config into container works
-cp -f ${LOCALHOST_HOME}/git/dotfiles/.gitconfig.linux ~/.gitconfig
-# ln -s -f ${LOCALHOST_HOME}/git/dotfiles/.gitconfig.linux ~/.gitconfig
+readonly LOCALHOST_HOME=/localhost_home
